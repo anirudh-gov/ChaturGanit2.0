ca735db9739b5a3f9c19999dc7ca318d5ea2e45e
Govardhana Anirudh <anirudhgovardhana@gmail.com>
2024-09-12 20:09:45 +0530

    Update app.js

--- app.js
+++ app.js
@@ -12,10 +12,6 @@ app.use(express.static(path.join(__dirname,"public")));
 app.use(express.urlencoded({extended:true}));
 app.use(methodOverride("_method"));
 
-app.get("/", (req,res)=>{
-    res.send("New Project root is Working!");
-});
-
 app.get("/Home", (req,res)=>{
     res.render("home.ejs");
 });
